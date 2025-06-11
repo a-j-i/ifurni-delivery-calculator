aa90dcdf3228881f73d03137bffacf86c1e1d280
Ajith <vaddevoluajithaj@gmail.com>
2025-06-11 18:33:10 +1000

    connected to github

---
+++ README.md
@@ -0,0 +1,1 @@
+Building a dashboard for managing jobs in a furniture company.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="bg-[#1E1E1E] text-white min-h-screen p-4">
+      </div>
+    </Router>
+  );
+}
+
+export default App;
